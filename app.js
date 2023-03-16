7c4ffddce091b77d94b6c1a334089d5163ec7672
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2023-03-16 19:17:51 -0400

    fix for rendering of stories, problem at handlebars dashboard

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ connectDB();
 
 const app = express();
 
+// body parser
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 // used for logging dev info to console
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -57,6 +61,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 8500;
 
